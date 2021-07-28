221f9b3e1329341445336159a315ec64715c6375
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-28 14:53:18 +0100

    maire

---
+++ cmake-build-debug/CMakeFiles/Phallus.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Phallus.dir/main.c.obj"
+  "Phallus.exe"
+  "Phallus.exe.manifest"
+  "Phallus.lib"
+  "Phallus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Phallus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
