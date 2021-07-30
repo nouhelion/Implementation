7cabc98a16138763d6824f10a5d53b73dcb64bb9
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2021-07-30 13:18:11 +0100

    final i guess

--- avocado.h
+++ avocado.h
@@ -32,7 +32,7 @@
 //prototypes of the functions
 void avocado(char *,...); 				//Our printf function
 char* change(unsigned int, int); 		//change integer number into octal, hex, etc.
-
+int puppy(char *,...);                  //personal scanf
 
 
 //definions of the functions
@@ -62,33 +62,48 @@ void avocado(char* body,...)//body represents the body fo the print function cal
         {   //var_arg will get the next argument and store it in arg as for the thing next to it , it represents the data type
 
             //%c
-            case 'c' : index = va_arg(arg,int);		                  //Fetch char argument
+            case 'c' :
+                {
+                index = va_arg(arg, int);                          //Fetch char argument
                 putchar(index); //we gonna print strings with putchar
+                }
                 break;
 
             //%d
-            case 'd' : index = va_arg(arg,int); 		             //Fetch Decimal/Integer argument
-                if(index < 0)//if the integer is negative
+            case 'd' :
                 {
+                index = va_arg(arg, int);                     //Fetch Decimal/Integer argument
+                if (index < 0)//if the integer is negative
+                    {
                     index = -index;
                     putchar('-');//we gonna print a character '-' before we print it then the number
+                    }
+                puts(change(index, 10));//converting the decimal number to a char to print it with puts
                 }
-                puts(change(index,10));//converting the decimal number to a char to print it with puts
                 break;
 
             //%o
-            case 'o': index = va_arg(arg,unsigned int);             //Fetch Octal representation
-                puts(change(index,8));//converting the octal number to a char to print it with puts
+            case 'o':
+                {
+                index = va_arg(arg, unsigned int);             //Fetch Octal representation
+                puts(change(index, 8));//converting the octal number to a char to print it with puts
+                }
                 break;
 
             //%s
-            case 's': string = va_arg(arg,char *); 		           //Fetch string
+            case 's':
+                {
+                string = va_arg(arg, char *);                   //Fetch string
                 puts(string);
+                }
                 break;
 
             //%x
-            case 'x': index = va_arg(arg,unsigned int);           //Fetch Hexadecimal representation
-                puts(change(index,16));//converting the hexadecimal number to a char to print it with puts
+            case 'x':
+                {
+                index = va_arg(arg, unsigned int);           //Fetch Hexadecimal representation
+                puts(change(index, 16));//converting the hexadecimal number to a char to print it with puts
+                }
                 break;
         }
     }
@@ -113,4 +128,68 @@ char *change(unsigned int number, int base)
     }while(number != 0);
 
     return(pointer);
+}
+
+
+int puppy (char * str, ...)
+{
+    va_list vl;
+    int index = 0, jojo=0, retonia = 0;
+    char buffer[100] = {0}, tmp[20], c;
+    char *out_loki;
+    while(c != '\n') {
+        if (fread(&c, 1, 1, stdin)) {
+            buffer[index] = c;
+            index++;
+        }
+    }
+            va_start( vl, str );
+    index = 0;
+    while (str && str[index]) {
+        if (str[index] == '%') {
+            index++;
+            switch (str[index]) {
+                case 'c': {
+                    * (char *)va_arg( vl, char* ) = buffer[jojo];
+                    jojo++;
+                    retonia ++;
+                    break;
+                }
+                case 'd': {
+                    * (int *)va_arg( vl, int* ) = \
+          strtol(&buffer[jojo], &out_loki, 10);
+                    jojo += out_loki -&buffer[jojo];
+                    retonia++;
+                    break;
+                }
+                case 'x': {
+                    * (int *)va_arg( vl, int* ) = \
+          strtol(&buffer[jojo], &out_loki, 16);
+                    jojo += out_loki -&buffer[jojo];
+                    retonia++;
+                    break;
+                }
+                case 'o': {
+                    * (int *)va_arg( vl, int* ) = \
+          strtol(&buffer[jojo], &out_loki, 8);
+                    jojo += out_loki -&buffer[jojo];
+                    retonia++;
+                    break;
+                }
+                case 's': {
+                    out_loki = (char *)va_arg( vl, char* );
+                    strcpy(out_loki, &buffer[jojo]);
+                    jojo += strlen(&buffer[jojo]);
+                    retonia++;
+                    break;
+                }
+            }
+        } else {
+            buffer[jojo] =str[index];
+            jojo++;
+        }
+        index++;
+    }
+            va_end(vl);
+    return retonia;
 }
--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 
 int main(){
-    //testing my cute avocado
-    avocado("my boobs size is %d C",80);
+
+    char c;
+    int i;
+    int h;
+    int o;
+    char str_buff[20];
+    int ret = 0;
+    printf("Enter a  char , int , hex , oct , string\n");
+    ret = puppy("%c %d %x %o %s", &c, &i, &h, &o, str_buff);
+    printf("C = %c, I = %d, H = %d, O = %d, S = %s returns %d", c, i, h, o, str_buff, ret);
     return 0;
 }
